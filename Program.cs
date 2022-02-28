9b3dc124c95e54e9b8c3139673050baa6d924f7e
KerinAO <90246117+KerinAO@users.noreply.github.com>
2022-02-28 19:37:37 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasljeđivanje
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Bijelogoricno hrast = new Bijelogoricno();
+            Crnogoricno bor = new Crnogoricno();
+
+            Console.WriteLine($"Hrast : {hrast.OpadajuListovi}");
+            Console.WriteLine($"Bor : {bor.OpadajuListovi}");
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Biljka
+    {
+    }
+
+    public class Stablo : Biljka
+    {
+        bool opadajuListovi;
+        public bool OpadajuListovi { get => opadajuListovi; set => opadajuListovi = value; }
+
+        public Stablo(bool arg_opadajuListovi)
+        {
+            opadajuListovi = arg_opadajuListovi;
+        }
+    }
+
+    public class Cvijet : Biljka { }
+
+    public class Bijelogoricno : Stablo
+    {
+        public Bijelogoricno(bool opadajuListovi = true) : base(opadajuListovi) { }
+    }
+
+    public class Crnogoricno : Stablo
+    {
+        public Crnogoricno(bool opadajuListovi = false) : base(opadajuListovi) { }
+    }
+
+
+   
+}
